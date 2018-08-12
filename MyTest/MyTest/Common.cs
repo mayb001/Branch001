4aede21c651fff8deba73fd7712ce638486444da
mayb001 <mayingbin88@263.com>
2018-08-12 11:31:14 +0800

    test

---
+++ MyTest/MyTest/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+    public class Common
+    {
+        public static string str = "Hello World !";
+    }
+}
